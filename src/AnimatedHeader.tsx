b62db80a3151156ee8c0d1c4867ae8b94ed2f6bb
Fahad Ibrahim <fibrahim@ssidecisions.com>
2024-07-25 19:47:28 +0500

    feat: implement feature for landscape orientation (Release 0.1.1)

--- src/AnimatedHeader.tsx
+++ src/AnimatedHeader.tsx
@@ -1,122 +1,188 @@
-import React, { useState } from 'react';
-import { Animated, Dimensions, Text, View, type StyleProp, type ViewStyle, } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  type StyleProp,
+  type ViewStyle,
+} from 'react-native';
 import ImageWithFallback from './ImageWithFallback';
 
-const { width: screenWidth } = Dimensions.get('window');
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
 interface MyComponentProps {
-    scrollY: Animated.Value;
-    leftComponentStyle?: StyleProp<ViewStyle>;
-    LeftComponent: React.ReactNode;
-    rightComponentStyle?: StyleProp<ViewStyle>;
-    RightComponent: React.ReactNode;
+  scrollY: Animated.Value;
+  leftComponentStyle?: StyleProp<ViewStyle>;
+  LeftComponent: React.ReactNode;
+  rightComponentStyle?: StyleProp<ViewStyle>;
+  RightComponent: React.ReactNode;
+  orientation: string;
 }
 
-const AnimatedHeader: React.FC<MyComponentProps> = ({ scrollY, leftComponentStyle, LeftComponent, rightComponentStyle, RightComponent }) => {
+const AnimatedHeader: React.FC<MyComponentProps> = ({
+  scrollY,
+  leftComponentStyle,
+  LeftComponent,
+  rightComponentStyle,
+  RightComponent,
+  orientation,
+}) => {
+  const [HeaderHeight] = useState<number>(100);
+  const [systemWidth, setSystemWidth] = useState(screenWidth);
 
-    const [HeaderHeight] = useState<number>(100);
+  useEffect(() => {
+    if (orientation === 'portraint') {
+      setSystemWidth(screenWidth);
+    } else {
+      setSystemWidth(screenHeight);
+    }
+  }, [orientation]);
 
-    const headerHeight = scrollY.interpolate({
-        inputRange: [0, 100],
-        outputRange: [HeaderHeight, HeaderHeight - 30],
-        extrapolate: 'clamp',
-    });
+  const headerHeight = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [HeaderHeight, HeaderHeight - 30],
+    extrapolate: 'clamp',
+  });
 
-    const headerScale = scrollY.interpolate({
-        inputRange: [0, 100],
-        outputRange: [1, 0.5], // Scaling from 100% to 50%
-        extrapolate: 'clamp',
-    });
+  const headerScale = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [1, 0.5], // Scaling from 100% to 50%
+    extrapolate: 'clamp',
+  });
 
-    const headerTranslateX = scrollY.interpolate({
-        inputRange: [0, 100],
-        outputRange: [0, ((-screenWidth / 2) + 25 + (screenWidth * 0.15))], // Adjust 50 based on half of the width of the header
+  const headerTranslateX = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, -systemWidth / 2 + 25 + systemWidth * 0.15], // Adjust 50 based on half of the width of the header
 
-        extrapolate: 'clamp',
-    });
+    extrapolate: 'clamp',
+  });
 
-    const headerTextTranslateX = scrollY.interpolate({
-        inputRange: [0, 100],
-        outputRange: [(-screenWidth / 2), (-screenWidth / 2 + (screenWidth * 0.30) + 50 + 10)], // Adjust 50 based on half of the width of the header
+  const headerTextTranslateX = scrollY.interpolate({
+    inputRange: [0, 100],
+    outputRange: [
+      -systemWidth / 2,
+      -systemWidth / 2 + systemWidth * 0.3 + 50 + 10,
+    ], // Adjust 50 based on half of the width of the header
 
-        extrapolate: 'clamp',
-    });
+    extrapolate: 'clamp',
+  });
 
-    const headerTextOpacity = scrollY.interpolate({
-        inputRange: [0, 80, 100],
-        outputRange: [0, 0, 1],
-    });
+  const headerTextOpacity = scrollY.interpolate({
+    inputRange: [0, 80, 100],
+    outputRange: [0, 0, 1],
+  });
 
+  return (
+    <Animated.View
+      style={[
+        styles.container,
+        {
+          height: headerHeight,
+        },
+      ]}
+    >
+      <View
+        style={[
+          styles.leftView,
+          {
+            width: systemWidth * 0.15,
+          },
+          leftComponentStyle,
+        ]}
+      >
+        {LeftComponent}
+      </View>
 
-    return (
-        <Animated.View style={[{
-
-            flexDirection: 'row',
-            width: '100%',
-            height: headerHeight,
-        }]}>
-            <View style={[{
-                width: screenWidth * 0.15,
-                backgroundColor: 'orange'
+      <View
+        style={[
+          styles.centerView,
+          {
+            width: systemWidth * 0.7,
+          },
+        ]}
+      >
+        <Animated.View
+          style={[
+            styles.centerView2,
+            {
+              opacity: headerTextOpacity,
+              transform: [{ translateX: headerTextTranslateX }],
             },
-                leftComponentStyle]}>
-                {LeftComponent}
-            </View>
+          ]}
+        >
+          <Text
+            numberOfLines={1}
+            style={[styles.centerViewTitle, { width: systemWidth * 0.3 }]}
+          >
+            Header Title asdfasdfasd asd f asd fa sdf asd fa sdf asd fa sdf asd
+            f
+          </Text>
+        </Animated.View>
 
-            <View style={{
-                width: screenWidth * 0.7,
-                backgroundColor: 'blue',
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}>
-
-
-                <Animated.View style={
-                    {
-                        position: 'absolute',
-                        opacity: headerTextOpacity,
-                        transform: [
-                            { translateX: headerTextTranslateX },
-                        ],
-
-                        backgroundColor: 'skyblue',
-
-                    }}>
-                    <Text numberOfLines={1} style={{ fontSize: 16, width: screenWidth * 0.30 }}>Header Title  asdfasdfasd asd f asd fa sdf asd fa sdf asd fa sdf asd f</Text>
-                </Animated.View>
-
-                <Animated.View style={
-                    {
-
-                        transform: [
-                            { translateX: headerTranslateX, },
-                            { scale: headerScale }
-                        ],
-                        backgroundColor: 'skyblue',
-                        width: 100,
-                        height: 100,
-                    }}>
-                    <ImageWithFallback
-                        source={{ uri: 'https://www.w3schools.com/w3images/avatar2.png' }}
-                        style={{
-                            width: 100,
-                            height: 100,
-                            borderRadius: 50,
-                        }}
-                        fallbackSource={require('./assets/placeholderimg.png')}
-                    />
-
-                </Animated.View>
-
-            </View>
-            <View style={[{ width: screenWidth * 0.15, backgroundColor: 'orange' },
-                rightComponentStyle
-            ]}>
-                {RightComponent}
-
-            </View>
-        </Animated.View >
-    );
+        <Animated.View
+          style={[
+            styles.centerViewImageContainer,
+            {
+              transform: [
+                { translateX: headerTranslateX },
+                { scale: headerScale },
+              ],
+            },
+          ]}
+        >
+          <ImageWithFallback
+            source={{ uri: 'https://www.w3schools.com/w3images/avatar2.png' }}
+            style={[styles.centerViewImageBox]}
+            fallbackSource={require('./assets/placeholderimg.png')}
+          />
+        </Animated.View>
+      </View>
+      <View
+        style={[
+          styles.rightView,
+          { width: systemWidth * 0.15 },
+          rightComponentStyle,
+        ]}
+      >
+        {RightComponent}
+      </View>
+    </Animated.View>
+  );
 };
 
 export default AnimatedHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  leftView: { backgroundColor: 'orange' },
+  centerView2: {
+    position: 'absolute',
+    backgroundColor: 'skyblue',
+  },
+  centerViewTitle: {
+    fontSize: 16,
+  },
+  centerViewImageContainer: {
+    backgroundColor: 'skyblue',
+    width: 100,
+    height: 100,
+  },
+  centerView: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  centerViewImageBox: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  rightView: {
+    backgroundColor: 'orange',
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,29 @@ interface MainAnimatedHeaderProps {
   LeftComponent: React.ReactNode;
   rightComponentStyle?: StyleProp<ViewStyle>;
   RightComponent: React.ReactNode;
-};
+  orientation: string;
+}
 
-const MainAnimatedHeader: React.FC<MainAnimatedHeaderProps> = ({ scrollY, leftComponentStyle, LeftComponent, rightComponentStyle, RightComponent }) => {
-
+const MainAnimatedHeader: React.FC<MainAnimatedHeaderProps> = ({
+  scrollY,
+  leftComponentStyle,
+  LeftComponent,
+  rightComponentStyle,
+  RightComponent,
+  orientation,
+}) => {
   return (
     <View>
-      <AnimatedHeader scrollY={scrollY} LeftComponent={LeftComponent} leftComponentStyle={leftComponentStyle} RightComponent={RightComponent} rightComponentStyle={rightComponentStyle} />
+      <AnimatedHeader
+        scrollY={scrollY}
+        LeftComponent={LeftComponent}
+        leftComponentStyle={leftComponentStyle}
+        RightComponent={RightComponent}
+        rightComponentStyle={rightComponentStyle}
+        orientation={orientation}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default MainAnimatedHeader;
